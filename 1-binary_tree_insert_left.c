f827c7ab5c95418da9ede6553dc2bd4253cb5b19
standardgit <standardblaise@gmail.com>
2022-09-21 23:37:45 +0100

    binary trees assign left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -37,8 +37,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = new_node;
 		new_node->left = formal_left_node;
 	}
-
-	parent->left = new_node;
+	else
+	{
+		parent->left = new_node;
+	}
 
 	return (new_node);
 }
