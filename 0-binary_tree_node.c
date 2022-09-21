a40f1b78618a782cd1ed81e85eb121bac111e4ba
standardgit <standardblaise@gmail.com>
2022-09-21 22:25:54 +0100

    binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * *binary_tree_node - This function takes a binary tree and returns a pointer to it.
+ *	
+ * @parent: The binary tree.
+ * @value: The value of the binary tree.
+ * Return: A pointer to the binary tree.
+ */
+
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
+
 	if (temp == NULL)
 	{
 		return (NULL);
