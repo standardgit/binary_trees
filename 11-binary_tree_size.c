ce437f49bed61d0ee000799e97e69a9215d60db7
standardgit <standardblaise@gmail.com>
2022-09-22 23:25:57 +0100

    Binary tree ordering

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -22,6 +22,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
 
 	size_l = tree->left ? binary_tree_size(tree->left) : 0;
-	size_r = tree->right? binary_tree_size(tree->right): 0;
+	size_r = tree->right ? binary_tree_size(tree->right) : 0;
 	return (size_l + size_r + 1);
 }
